de2dddd33aa5d9ae385b978ee4da0244e11492d0
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-05-12 14:19:18 +0700

    tambah transactionFactory

---
+++ project_agile_kelas/Factory/TransactionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project_agile_kelas.Factory
+{
+    public class TransactionFactory
+    {
+
+    }
+}
