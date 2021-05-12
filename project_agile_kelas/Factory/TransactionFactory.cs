5ee717fa9f6f2c368df0627766918a10107b9ad8
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2021-05-12 14:32:03 +0700

    facotryUpdate ver1

--- project_agile_kelas/Factory/TransactionFactory.cs
+++ project_agile_kelas/Factory/TransactionFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using project_agile_kelas.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,15 @@ namespace project_agile_kelas.Factory
 {
     public class TransactionFactory
     {
+        public static TransactionHeader Create(int id, int typeId, string description, int price)
+        {
+            TransactionHeader t = new TransactionHeader();
+            t.transactionId = id;
+            t.transactionTypeId = typeId;
+            t.itemDescription = description;
+            t.price = price;
+            return t;
+        }
 
     }
 }
