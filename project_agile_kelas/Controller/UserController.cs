7017eb483129cd65ff8a612c919713d4b63a7255
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-05-12 14:51:03 +0700

    bikin controller :D

---
+++ project_agile_kelas/Controller/UserController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project_agile_kelas.Controller
+{
+    public class UserController
+    {
+    }
+}
