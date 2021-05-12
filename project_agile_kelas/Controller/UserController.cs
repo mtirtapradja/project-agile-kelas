44db6699ac8b3d89be5930a8d3346b507f1ae18d
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-05-12 15:09:11 +0700

    update usercontroller

--- project_agile_kelas/Controller/UserController.cs
+++ project_agile_kelas/Controller/UserController.cs
@@ -7,5 +7,38 @@ namespace project_agile_kelas.Controller
 {
     public class UserController
     {
+        public static String CheckLogin(string username, string password)
+        {
+            string response = "";
+
+            if (username == "")
+            {
+                response = "username must be filled";
+            }
+            else if(password == "")
+            {
+                response = "password must be filled";
+            }
+            return response;
+        }
+
+        public static String CheckRegister(string username, string password, string email)
+        {
+            string response = "";
+
+            if (username == "")
+            {
+                response = "username must be filled";
+            }
+            else if (password == "")
+            {
+                response = "password must be filled";
+            }
+            else if(email == "")
+            {
+                response = "email must be filled";
+            }
+            return response;
+        }
     }
 }
