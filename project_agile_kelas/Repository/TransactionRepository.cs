74a51d22d8841b5bcd1fe384bc76be2e8c8407dd
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2021-05-12 14:50:38 +0700

    bikinRepositoryTransaction

--- project_agile_kelas/Repository/TransactionRepository.cs
+++ project_agile_kelas/Repository/TransactionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using project_agile_kelas.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,46 @@ namespace project_agile_kelas.Repository
 {
     public class TransactionRepository
     {
+        private static DatabaseEntities db = new DatabaseEntities();
+
+        public static bool InsertTransaction(TransactionHeader newTransaction)
+        {
+            if (newTransaction != null)
+            {
+                db.TransactionHeaders.Add(newTransaction);
+                db.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool UpdateTransaction(TransactionHeader currTransaction)
+        {
+
+            if (currTransaction != null)
+            {
+                db.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteTransaction(TransactionHeader currTransaction)
+        {
+            if (currTransaction != null)
+            {
+                db.TransactionHeaders.Remove(currTransaction);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
     }
 }
